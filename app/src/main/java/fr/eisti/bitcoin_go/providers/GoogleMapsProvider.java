0ff20f633611649abc7e5363143ec5fd3a54ac81
alex <vacheretal@eisti.eu>
2017-11-06 14:38:38 +0100

    fin google maps

--- app/src/main/java/fr/eisti/bitcoin_go/providers/GoogleMapsProvider.java
+++ app/src/main/java/fr/eisti/bitcoin_go/providers/GoogleMapsProvider.java
@@ -2,7 +2,6 @@ package fr.eisti.bitcoin_go.providers;
 
 import android.location.Address;
 import android.location.Geocoder;
-import android.util.Pair;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -13,6 +12,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.io.IOException;
 import java.util.List;
 
+import fr.eisti.bitcoin_go.data.Location;
 import fr.eisti.bitcoin_go.maps.MapsActivity;
 
 public class GoogleMapsProvider {
@@ -26,25 +26,37 @@ public class GoogleMapsProvider {
     public void printResult(String name, String localisation, GoogleMap googleMap) {
         Geocoder geocoder = new Geocoder(mapsActivity);
 
-        List<Pair<Double, Double>> tmp;
+        if (name != null) {
 
-        if (localisation != null) {
+            //TODO
+            Location location = ...;
+
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            googleMap.addMarker(new MarkerOptions().position(latLng));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            googleMap.animateCamera(CameraUpdateFactory.zoomTo(14.0f));
+
+        } else if (localisation != null) {
             try {
-                List<Address> results = geocoder.getFromLocationName(localisation, 3);
-                LatLng originalLatLng = new LatLng(results.get(0).getLatitude(), results.get(0).getLongitude());
+                List<Address> results = geocoder.getFromLocationName(localisation, 1);
                 if (results.size() != 0) {
-                    for (Address address : results) {
+                    LatLng originalLatLng = new LatLng(results.get(0).getLatitude(), results.get(0).getLongitude());
+
+                    //TODO
+                    List<Location> locationList = ...;
+
+                    for (Location address : locationList) {
                         LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-                        googleMap.addMarker(new MarkerOptions().position(latLng).title("Marker in XXX"));
+                        googleMap.addMarker(new MarkerOptions().position(latLng));
                     }
                     googleMap.moveCamera(CameraUpdateFactory.newLatLng(originalLatLng));
                     googleMap.animateCamera(CameraUpdateFactory.zoomTo(14.0f));
                 } else
-                    Toast.makeText(mapsActivity, "Auncun résultat trouvé", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(mapsActivity, "Aucun résultat trouvé", Toast.LENGTH_SHORT).show();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        } else Toast.makeText(mapsActivity, "Localisation nulle", Toast.LENGTH_SHORT).show();
+        } else Toast.makeText(mapsActivity, "Localisation et nom null", Toast.LENGTH_SHORT).show();
     }
 
 }
