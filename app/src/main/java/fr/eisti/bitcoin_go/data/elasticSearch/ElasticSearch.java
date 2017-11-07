609b7d0482d863e3c020805038330433b6f1d021
erwanlbp <erwan.lbp@gmail.com>
2017-11-07 14:12:45 +0100

    o

--- app/src/main/java/fr/eisti/bitcoin_go/data/elasticSearch/ElasticSearch.java
+++ app/src/main/java/fr/eisti/bitcoin_go/data/elasticSearch/ElasticSearch.java
@@ -4,23 +4,32 @@ import android.content.Context;
 import android.util.Log;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
 import fr.eisti.bitcoin_go.data.Bitcoin;
 
 public class ElasticSearch {
+    public final static String URL = "https://urautoiymb:lwypa9mr9a@bitcoin-go-3784488190.eu-west-1.bonsaisearch.net/requests/data/";
 
     public static final String TAG = "##### ELASTIC SEARCH";
+
+    private static RequestQueue queue;
 
     public static void insertData(Context context, Bitcoin bitcoin) {
         Log.i(TAG, "Insert : " + bitcoin.getName());
 
+        if (queue == null) {
+            queue = Volley.newRequestQueue(context);
+        }
+
         JsonObjectRequest jsObjRequest = new JsonObjectRequest
-                (Request.Method.POST, SearchSingleton.URL + bitcoin.getName(), bitcoin.toJSONObject(), new Response.Listener<JSONObject>() {
+                (Request.Method.GET, URL + bitcoin.getName(), bitcoin.toJSONObject(), new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
@@ -30,12 +39,12 @@ public class ElasticSearch {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.e(TAG, error.getMessage());
+                        Log.e(TAG, error.toString());
                     }
                 });
 
         // Access the RequestQueue through your singleton class.
-        SearchSingleton.getInstance(context).addToRequestQueue(jsObjRequest);
+        queue.add(jsObjRequest);
 
     }
 
